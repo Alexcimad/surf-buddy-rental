ee22bdfcc14fbf70a4a270617371b3e5525dec34
Alexis Cimadevilla <alex.cimadev@gmail.com>
2022-02-02 23:20:52 +0100

    bookings creation done

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,5 +1,5 @@
 class Booking < ApplicationRecord
   belongs_to :offer
   belongs_to :user
-  validates  :total_price, :start_date, :end_date, presence: true
+  validates  :total_price, :start_rental_date, :end_rental_date, presence: true
 end
