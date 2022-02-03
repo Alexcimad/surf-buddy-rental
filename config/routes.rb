d25cbd571be1f7504b054f33fdb0a7a678376a01
theo galais <theogalais@gmail.com>
2022-02-03 20:05:48 +0100

    Add Crud in Offers

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :offers, only: [:index, :new, :create, :show] do
+  resources :offers, only: [:index, :new, :create, :show, :update, :edit, :destroy] do
     resources :bookings, only: [:new, :create]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
