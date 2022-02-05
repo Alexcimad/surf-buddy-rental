56070a21548c1b94a5b590189984074a8e0d968a
Alexis Cimadevilla <alex.cimadev@gmail.com>
2022-02-05 14:32:30 +0100

    add state column to offers

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -54,7 +54,7 @@ class OffersController < ApplicationController
   private
 
   def offer_params
-    params.require(:offer).permit(:title, :description, :price_per_date, :start_available_date, :location, :end_available_date, photos: [])
+    params.require(:offer).permit(:title, :description, :price_per_date, :start_available_date, :location, :state, :end_available_date, photos: [])
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_05_101424) do
+ActiveRecord::Schema.define(version: 2022_02_05_132310) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 2022_02_05_101424) do
     t.string "location"
     t.float "latitude"
     t.float "longitude"
+    t.string "state"
     t.index ["user_id"], name: "index_offers_on_user_id"
   end
 
