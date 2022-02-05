9f5ebdfbbbd8071805744e3ebad5ba900fe8a691
Alexis Cimadevilla <alex.cimadev@gmail.com>
2022-02-05 12:12:09 +0100

    V1 version of geocoding implemented

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_03_201635) do
+ActiveRecord::Schema.define(version: 2022_02_05_101424) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,8 @@ ActiveRecord::Schema.define(version: 2022_02_03_201635) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "location"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_offers_on_user_id"
   end
 
