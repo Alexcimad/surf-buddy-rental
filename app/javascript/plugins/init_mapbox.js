70725ac759229d6a5ad482d77165fdfdc673c6de
Alexis Cimadevilla <alex.cimadev@gmail.com>
2022-02-09 16:45:49 +0100

    add info window to map

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -12,8 +12,10 @@ const buildMap = (mapElement) => {
   
   const addMarkersToMap = (map, markers) => {
     markers.forEach((marker) => {
+      const popup = new mapboxgl.Popup().setHTML(marker.info_window); // add this
       new mapboxgl.Marker()
         .setLngLat([ marker.lng, marker.lat ])
+        .setPopup(popup) // add this
         .addTo(map);
     });
   };
