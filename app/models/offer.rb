8ea6b044b08d411ff9d7d5914477e1d95688149e
Alexis Cimadevilla <alex.cimadev@gmail.com>
2022-02-09 23:23:41 +0100

    add reviews in index and show: css to improve

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -6,4 +6,13 @@ class Offer < ApplicationRecord
   has_many_attached :photos
   validates :title, :description, :price_per_date, :state, :location, presence: true
   after_validation :geocode, if: :will_save_change_to_location?
+
+  def ratings_average
+    sum = 0
+    reviews.each do |review|
+      sum += review.rating
+    end
+    return sum.fdiv(reviews.count).ceil(1)
+  end
+
 end
